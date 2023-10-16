613342057221a50b924ce9eafbcc6466e2a84712
Khushboo48 <85827765+Khushboo48@users.noreply.github.com>
2023-10-16 22:23:05 +0530

    Add files via upload

---
+++ Ride-Sharing-application/instacar-master/templates/char.java
@@ -0,0 +1,29 @@
+public class char {
+     public static void main char (String[]args)
+    {
+        char c = 'f';
+
+        // show error message if c is not an alphabet
+        // if (!isalpha (c))
+        //   System.out.println  ("Non alphabet");
+
+        if (isLowercaseVowel (c) || isUppercaseVowel (c))
+            System.out.println (c + " is a vowel ");
+
+        else
+            System.out.println (c + " is a consonant ");
+
+    }
+
+    static boolean isLowercaseVowel (char c)
+    {
+        // returns 1 if char matches any of below
+        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    }
+
+    static boolean isUppercaseVowel (char c)
+    {
+        // returns 1 if char matches any of below
+        return (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+    }
+}
---
+++ Ride-Sharing-application/instacar-master/templates/first.java
@@ -0,0 +1,18 @@
+public class first {
+    public static viod main(string args[]){
+         int[]input array = (10,58,37,89);
+          int[]largest = input Array[0];
+          int[]smallest = input Array[0];
+          for(int number: input array){
+             if (number>largest);
+              largest=number;
+
+          }
+          else if (smallest>number){
+            smallest=number;
+            
+          }
+    }
+    
+    
+}
